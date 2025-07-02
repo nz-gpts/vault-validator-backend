4c4692e85ab45c853093825e8a988712cd8e2f2e
neckahacka <amynztoday@gmail.com>
2025-07-02 14:38:38 +1200

    confirm working vaultctl health, clean router

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
+from vaultctl import router as vaultctl_router
 
 app = FastAPI()
+
+# include the vaultctl router with prefix
+app.include_router(vaultctl_router, prefix="/vaultctl")
 
 @app.get("/")
 def read_root():
